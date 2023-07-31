bc89554a3b7e8250acf4eeb781ba86d7c47c30f0
Phillip Jhon Lumod <44081207+philliplumod@users.noreply.github.com>
2023-07-31 13:23:12 +0800

    crud

--- lib/data/model/note_model.dart
+++ lib/data/model/note_model.dart
@@ -7,6 +7,15 @@ class NoteFields {
   static const String title = 'title';
   static const String description = 'description';
   static const String time = 'time';
+
+  static final List<String> values = [
+    id,
+    isImportant,
+    number,
+    title,
+    description,
+    time
+  ];
 }
 
 class Note {
@@ -25,4 +34,38 @@ class Note {
     required this.description,
     required this.createdTime,
   });
+
+  Note copy(
+          {int? id,
+          bool? isImportant,
+          int? number,
+          String? title,
+          String? description,
+          DateTime? createdTime}) =>
+      Note(
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        createdTime: createdTime ?? this.createdTime,
+      );
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        isImportant: json[NoteFields.isImportant] == 1,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        description: json[NoteFields.description] as String,
+        createdTime: DateTime.parse(json[NoteFields.time] as String),
+      );
+
+  Map<String, Object?> toJson() => {
+        NoteFields.id: id,
+        NoteFields.isImportant: isImportant ? 1 : 0,
+        NoteFields.number: number,
+        NoteFields.title: title,
+        NoteFields.description: description,
+        NoteFields.time: createdTime.toIso8601String(),
+      };
 }
